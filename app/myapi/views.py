9ff91fac6c99b542d1aa4574cb86b51d7197cd5b
Luis Arce <luis.arce22@gmail.com>
2020-04-17 21:53:32 -0400

    add permission to Hero api

--- app/myapi/views.py
+++ app/myapi/views.py
@@ -5,11 +5,14 @@ from .serializers import HeroSerializer
 from .serializers import ProjectSerializer
 from .models import Hero
 from .models import Project
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 
 class HeroViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    
     queryset = Hero.objects.all().order_by('name')
     serializer_class = HeroSerializer
 
