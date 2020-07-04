863516f0b2f9fa814c6bb720e595499762f78843
Luis Arce <luis.arce22@gmail.com>
2020-07-03 22:14:01 -0400

    Update urls.py

--- app/myapi/urls.py
+++ app/myapi/urls.py
@@ -18,7 +18,7 @@ router.register(r'members', views.TeamMemberViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
